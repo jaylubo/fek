34f40c39eff93907619d9d65b4fb10d072f95f2f
jlubomir <jlubomir@cisco.com>
2018-02-08 10:28:23 -0500

    parse syslogs more

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 
 apt-get update
-apt-get install -y openjdk-8-jre-headless apt-transport-https elasticsearch kibana
+apt-get install -y openjdk-8-jre-headless apt-transport-https elasticsearch kibana build-essential ruby ruby-dev
 
 curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-xenial-td-agent3.sh | sh
 
@@ -17,6 +17,8 @@ cp /vagrant/kibana.yml /etc/kibana/
 /bin/systemctl start kibana.service
 
 /usr/sbin/td-agent-gem install fluent-plugin-elasticsearch --no-document
+/usr/bin/gem install fluent-plugin-rewrite-tag-filter
+/usr/sbin/td-agent-gem install fluent-plugin-parser
 cp /vagrant/td-agent.conf /etc/td-agent/
 systemctl enable td-agent
 systemctl start td-agent
